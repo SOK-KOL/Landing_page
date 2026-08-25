4865b0836ccfbafc5ca7fbb4a2c613439dde71f2
Evgeniy <ze.agafonow@gmail.com>
2026-08-25 15:39:41 +0300

    changed:   scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,11 @@ const header = document.querySelector('header');
 const burger = document.querySelector('.burger');
 const cookies = document.querySelector('.cookies');
 const cookiseBtn = document.querySelector('.cookies__button');
+const form = document.querySelector('#form');
+
+if (form) {
+    form.action = atob(form.dataset.formEndpoint);
+}
 
 burger.addEventListener('click', function (e) {
     header.classList.toggle("header--open")
